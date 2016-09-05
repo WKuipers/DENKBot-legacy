eb9605013b581a84ea592a8bd9db3cfcf890c03f
WKuipers <wietse@kuiprs.nl>
2016-09-05 18:51:31 +0200

    Modularizing done right

--- DENKbot.py
+++ DENKbot.py
@@ -3,7 +3,7 @@ import asyncio
 import os
 import json
 
-import memes
+import modules.memes as memes
 
 if 'DISCORD_TOKEN' in os.environ:
     token = os.environ['DISCORD_TOKEN']
@@ -17,5 +17,5 @@ client = discord.Client()
 async def on_ready():
     print('Logged in as ' + client.user.name)
 
-memes.Mememodule(client)
+memes.MemesModule(client)
 client.run(token)
